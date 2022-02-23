5e91f8a006199f44299bbcd017a69196bbfbd0d5
SP7TECH <spandey247789@gmail.com>
2022-02-23 11:08:46 +0530

    Controller complete

--- backend/routes/goalsRoutes.js
+++ backend/routes/goalsRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+    protect
+} = require("../middleware/authMiddlware");
+
 const goalController = require("../controllers/goalController");
 
-router.route("/api/goals").get(goalController.getGoals).post(goalController.postGoals);
-router.route("/api/goals/:id").put(goalController.putGoals).delete(goalController.deleteGoals);
+router.route("/api/goals").get(protect, goalController.getGoals).post(protect, goalController.setGoal);
+router.route("/api/goals/:id").put(protect, goalController.updateGoal).delete(protect, goalController.deleteGoal);
 
 
 module.exports = router;
